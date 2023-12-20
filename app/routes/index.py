05e489dc5582eb2ae620db91f42c842a874f5614
Ildan <ildan201@yandex.ru>
2023-12-20 20:19:53 +0400

    change index page

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import Discussion
+from app.models import Discussion, Service, Case
 from app.forms import DiscussionForm
 from flask import url_for, render_template, redirect
 
@@ -8,6 +8,20 @@ from flask import url_for, render_template, redirect
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = DiscussionForm()
+    clservice: Service = Service()
+    clcase: Case = Case()
+    services = clservice.fetchall()
+    if services[0]:
+        services = services[1]
+    else:
+        services = []
+        
+    cases = clcase.fetchall()
+    if cases[0]:
+        cases = cases[1]
+    else:
+        cases = []
+
     if form.validate_on_submit():
         discussion: Discussion = Discussion()
         discussion.name = form.name.data
@@ -19,4 +33,4 @@ def index():
 
         return redirect(url_for('index'))
 
-    return render_template('/client/index.html', form=form)
+    return render_template('/client/index.html', services=services, cases=cases, form=form)
